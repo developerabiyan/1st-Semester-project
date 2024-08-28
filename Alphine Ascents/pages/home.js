fa8184a0c7162a2a38903694b805bf56bb783cce
HAFIZ ABIYAN <145971451+developerabiyan@users.noreply.github.com>
2024-08-28 18:58:08 +0500

    first

---
+++ Alphine Ascents/pages/home.js
@@ -0,0 +1,96 @@
+// nav bar hamburger icon script
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// words animation on front img
+
+var words = [
+    "Alphine Ascents",
+    "Discover",
+    "Highest Peek",
+    "Mountains",
+    "Shelters ",
+    "&",
+    "Hotels",
+    "Safety Hazards",
+    "&",
+    "Record breakers",
+    "geo locations",
+    "Maps and Weather",
+    "And many more",
+    "Knowledge",
+    "&",
+    "Information",
+    "About Mountains",
+    "On this",
+    "Website",
+    "Alphine Ascents",
+  ],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 15,
+  speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      } else {
+        offset--;
+      }
+    }
+    $(".word").text(part);
+  }, speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".col");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 250;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
